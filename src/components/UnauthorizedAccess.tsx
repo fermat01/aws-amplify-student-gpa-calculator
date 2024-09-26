72d81301b9c9c4ed1aec85151c6af5d91d949c17
vianney D <fermat01fils@gmail.com>
2024-09-26 09:30:25 +0200

    update react component

--- src/components/UnauthorizedAccess.tsx
+++ src/components/UnauthorizedAccess.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const UnauthorizedAccess: React.FC = () => (
-  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+ <div className="flex flex-col items-center justify-center min-h-[50vh] bg-gray-100 p-8 rounded-lg shadow-md mx-auto my-20 max-w-md">
     <h1 className="text-3xl font-bold text-red-600 mb-4">Unauthorized Access</h1>
     <p className="text-xl text-gray-700">You don't have the right to do that.</p>
   </div>
