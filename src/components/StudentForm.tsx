e16088c39fbabf58f006f8ffe233cf63bf246adc
vianney D <fermat01fils@gmail.com>
2024-10-04 11:46:03 +0200

    update component

--- src/components/StudentForm.tsx
+++ src/components/StudentForm.tsx
@@ -69,7 +69,7 @@ if (isAnyFieldEmpty) {
     try {
       // Send data to your API Gateway using Axios
  
-      const response = await axios.post("https://gme5586ir0.execute-api.us-east-1.amazonaws.com/dev", formData);
+      const response = await axios.post("https://w9m4f7x9re.execute-api.us-east-1.amazonaws.com/dev", formData);
 
       if (response.status === 200) {
         // Assuming your Lambda function returns the calculated GPA in response.data.gpa
